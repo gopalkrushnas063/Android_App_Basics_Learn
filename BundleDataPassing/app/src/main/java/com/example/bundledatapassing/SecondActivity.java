9427379d01fce66b8a09751ec343cdda22a2f42f
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-07-31 12:22:37 +0530

    Bundle Passing

---
+++ BundleDataPassing/app/src/main/java/com/example/bundledatapassing/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.bundledatapassing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent fromActivity = getIntent();
+        String title =  fromActivity.getStringExtra("title");
+        String StudentName =  fromActivity.getStringExtra("StudentName");
+        int RollNo =  fromActivity.getIntExtra("Roll No",0);
+
+        TextView txtStudentInfo;
+        txtStudentInfo = findViewById(R.id.txtStudentInfo);
+
+        txtStudentInfo.setText("Roll No : "+RollNo+", Name : "+StudentName);
+        getSupportActionBar().setTitle(title);
+    }
+}
