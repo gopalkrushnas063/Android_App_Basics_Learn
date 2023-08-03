9602cd3592431f1330ecccef8783463abd626f41
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-08-03 13:54:37 +0530

    Future Activity Frontend Added

---
+++ UIDesignProject/app/src/main/java/com/example/myapplication/Activitis/FutureActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.Activitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class FutureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_future);
+    }
+}
